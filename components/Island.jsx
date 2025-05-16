326b0fb9658742af8084b9cacd755c4a291dcb51
geebounce <geebounce@gmail.com>
2025-05-16 14:35:25 -0600

    Fixed Island image path

--- components/Island.jsx
+++ components/Island.jsx
@@ -3,7 +3,7 @@ import classes from "./Island.module.css";
 export default function Island() {
   return (
     <div className={classes.island_container}>
-      <img src="../public/isla.svg" alt="Island" />
+      <img src="/isla.svg" alt="Island" />
     </div>
   );
 }
